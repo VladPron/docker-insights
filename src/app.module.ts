60540e717d7dbd12cc757f0ef94d97a95224bf3c
vladislavpron0@gmail.com <>
2023-03-13 17:26:52 +0200

    chore: save changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { DockerInsightsModule } from './docker-insights/docker-insights.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DockerInsightsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
